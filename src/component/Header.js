cb15d6d5bd395d4c2777dc978739134478b5e8e9
gyeonggumun <lion989072@gmail.com>
2026-03-04 00:47:53 +0900

    오늘 날짜 랜더링 설정

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,5 +1,8 @@
 const Header = () => {
-    return <div className="Header">Header Component</div>
+    return <div className="Header">
+        <h3>오늘은 📅</h3>
+        <h1>{new Date().toDateString()}</h1>
+    </div>
 };
 
 export default Header;
