b480be708ba7b040d6ae3eec5eccb419c6cbb7e4
gyeonggumun <lion989072@gmail.com>
2026-03-05 00:13:10 +0900

    UI 수정

--- src/component/TodoEditor.js
+++ src/component/TodoEditor.js
@@ -1,5 +1,11 @@
 const TodoEditor = () => {
-    return <div className="TodoEditor">TodoEditor Component</div>
+    return <div className="TodoEditor">
+        <h4>새로운 Todo 작성하기 ✏️</h4>
+        <div className="editor_wrapper">
+            <input placeholder="새로운 Todo..." />
+            <button>추가</button>
+        </div>
+    </div>
 };
 
 export default TodoEditor;
