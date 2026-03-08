0145d0f549deed118414a061e70b8267d8220812
gyeonggumun <lion989072@gmail.com>
2026-03-08 20:55:09 +0900

    list에 Item컴포넌트 적용

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -1,9 +1,15 @@
+import TodoItem from "./TodoItem";
 import "./TodoList.css"
 
 const TodoList = () => {
     return <div className="TodoList">
         <h4>Todo List 🌱</h4>
         <input className="searchbar" placeholder="검색어를 입력해주세요" />
+        <div className="list_wrapper">
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
+        </div>
     </div>
 };
 
