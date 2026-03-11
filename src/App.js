20cf7aeee4b4f9aab1360075c9eed32f1b339656
gyeonggumun <lion989072@gmail.com>
2026-03-12 00:04:58 +0900

    todo상태 추가

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import './App.css';
 import Header from './component/Header';
 import TodoEditor from './component/TodoEditor';
 import TodoList from './component/TodoList';
 
 function App() {
+  const [todo, setTodo] = useState([]);
   return <div className="App">
     <Header />
     <TodoEditor />
