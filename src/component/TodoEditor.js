12525ada3f844786d87c8e07982996a2d8bd48cd
gyeonggumun <lion989072@gmail.com>
2026-03-15 20:30:26 +0900

    onsubmit에서 content값이 비어 있으면 입력 폼에 포커스를 구현

--- src/component/TodoEditor.js
+++ src/component/TodoEditor.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import "./TodoEditor.css"
 
 const TodoEditor = ({ onCreate }) => {
     const [content, setContent] = useState("");
+    const inputRef = useRef();
+
     const onChangeContent = (e) => {
         setContent(e.target.value);
     };
 
     const onSubmit = () => {
+        if (!content) {
+            inputRef.current.focus();
+            return;
+        }
         onCreate(content);
     };
 
@@ -15,6 +21,7 @@ const TodoEditor = ({ onCreate }) => {
         <h4>새로운 Todo 작성하기 ✏️</h4>
         <div className="editor_wrapper">
             <input
+                ref={inputRef}
                 value={content}
                 onChange={onChangeContent}
                 placeholder="새로운 Todo..." 
