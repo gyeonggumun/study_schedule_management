cf84fb966182fb50b295b6f23f48225c2a80e33a
gyeonggumun <lion989072@gmail.com>
2026-03-17 01:19:24 +0900

    enter키 눌렀을 때 꺼지도록 설정

--- src/component/TodoEditor.js
+++ src/component/TodoEditor.js
@@ -18,6 +18,12 @@ const TodoEditor = ({ onCreate }) => {
         setContent("");
     };
 
+    const onKeyDown = (e) => {
+        if (e.keyCode === 13) {
+            onSubmit();
+        }
+    };
+
     return <div className="TodoEditor">
         <h4>새로운 Todo 작성하기 ✏️</h4>
         <div className="editor_wrapper">
@@ -25,6 +31,7 @@ const TodoEditor = ({ onCreate }) => {
                 ref={inputRef}
                 value={content}
                 onChange={onChangeContent}
+                onKeyDown={onKeyDown}
                 placeholder="새로운 Todo..." 
             />
             <button onClick={onSubmit}>추가</button>
