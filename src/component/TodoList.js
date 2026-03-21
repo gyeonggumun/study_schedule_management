b733e3097aac976eb331742a810280d1c54bdd06
gyeonggumun <lion989072@gmail.com>
2026-03-21 14:53:59 +0900

    검색 폼에서 사용자가 입력한 내용을 처리하도록 기능추가

--- src/component/TodoList.js
+++ src/component/TodoList.js
@@ -2,9 +2,18 @@ import TodoItem from "./TodoItem";
 import "./TodoList.css"
 
 const TodoList = ({todo}) => {
+    const [search, setSearch] = useState("");
+    const onChangeSearch = (e) => {
+        setSearch(e.target.value);
+    };
     return <div className="TodoList">
         <h4>Todo List 🌱</h4>
-        <input className="searchbar" placeholder="검색어를 입력해주세요" />
+        <input 
+            value={search}
+            onChange={onChangeSearch}
+            className="searchbar" 
+            placeholder="검색어를 입력해주세요" 
+        />
         <div className="list_wrapper">
             {todo.map((it) => (
                 <TodoItem key={it.id} {...it} />
